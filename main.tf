7d487ff73882b831c9051b953fed132d0bad6614
rizhiy-site <98898941+rizhiy-site@users.noreply.github.com>
2025-09-22 13:29:32 +0200

    create new dev vpc

--- main.tf
+++ main.tf
@@ -18,12 +18,29 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west2a", "us-west2b", "us-west2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
   
+  subnet_id = module.blog.vpc.public_subnets[0]
+
   tags = {
     Name = "HelloWorld"
   }
@@ -34,7 +51,8 @@ module "blog_sg" {
   version = "5.3.0"
   name    = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
+  name   = "blog"
   
   ingress_rules      = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]

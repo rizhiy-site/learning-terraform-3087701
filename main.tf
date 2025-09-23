6d2e72ade4863dbd5b53fcb76b0549ea43828a61
= <=>
2025-09-23 11:57:55 +0200

    rollback and fix by claude

--- main.tf
+++ main.tf
@@ -14,13 +14,17 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
@@ -29,28 +33,26 @@ module "blog_vpc" {
   }
 }
 
-
-module "blog_autoscaling" {
+module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.0.1"
+  
+  name     = "blog"
+  min_size = 1
+  max_size = 2
 
-  name = "blog"
-
-  min_size            = 1
-  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
   security_groups     = [module.blog_sg.security_group_id]
-
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
+  
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 }
 
 # Attach the Auto Scaling Group to the target group
 resource "aws_autoscaling_attachment" "asg_attachment" {
-  autoscaling_group_name = module.blog_autoscaling.autoscaling_group_name
-  lb_target_group_arn   = module.blog_alb.target_groups["main"].arn
+  autoscaling_group_name = module.autoscaling.autoscaling_group_name
+  lb_target_group_arn   = module.blog_alb.target_groups["blog"].arn
 }
-
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
@@ -58,19 +60,16 @@ module "blog_alb" {
 
   name = "blog-alb"
 
-  load_balancer_type = "application"
-
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = {
-    main = {
-      name_prefix          = "main-"
-      backend_protocol     = "HTTP"
-      backend_port         = 80
-      target_type         = "instance"
-      deregistration_delay = 10
+    blog = {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
       health_check = {
         enabled             = true
         interval           = 30
@@ -92,7 +91,7 @@ module "blog_alb" {
         forward = {
           target_groups = [
             {
-              target_group_key = "main"
+              target_group_key = "blog"
               weight          = 100
             }
           ]
@@ -102,18 +101,22 @@ module "blog_alb" {
   }
 
   tags = {
-    Environment = "dev"
+    Environment = "Development"
+    Project     = "Example"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  # version = "4.13.0"
+  version = "5.3.0"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  vpc_id = module.blog_vpc.vpc_id
+  name   = "blog"
+  
+  ingress_rules      = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
+
+  egress_rules      = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
+
 }
